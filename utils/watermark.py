c9ea278a7f9eb97d94bf52c1d63a9614c3429471
unknown <you@example.com>
2023-02-27 19:25:38 +0800

    0227zyb

--- utils/watermark.py
+++ utils/watermark.py
@@ -1,21 +1,30 @@
 import numpy as np
 
 
-def get_X(net_glob, embed_dim, num_users):
+def get_X(net_glob, embed_dim, num_users, use_watermark=False):
     dict_X = {}
     p = net_glob.params()
     X_rows = p.numel()
     X_cols = embed_dim
+    if not use_watermark:
+        for i in range(num_users):
+            dict_X[i] = None
+        return dict_X
+
     for i in range(num_users):
         dict_X[i] = np.random.randn(X_rows, X_cols)
-
     return dict_X
 
 
-def get_b(embed_dim, num_users):
+def get_b(embed_dim, num_users, use_watermark=False):
     dict_b = {}
+    if not use_watermark:
+        for i in range(num_users):
+            dict_b[i] = None
+        return dict_b
     for i in range(num_users):
         dict_b[i] = np.random.randn(1, embed_dim)
+        dict_b[i] = (np.sign(dict_b[i])+1) / 2
     return dict_b
 
 
@@ -30,6 +39,6 @@ def get_layer_weights_and_predict(model, w):
 
 
 def compute_BER(pred_b, b):
-    correct_bit = np.logical_and(pred_b, b)
+    correct_bit = np.logical_not(np.logical_xor(pred_b, b))
     correct_bit_num = correct_bit.sum()
     return correct_bit_num / pred_b.size
